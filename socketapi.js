f3222e5b30317e457c4dec57695b532c53d1ebf2
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2024-02-21 19:59:56 +0530

    socketID saved in the database

--- socketapi.js
+++ socketapi.js
@@ -1,11 +1,30 @@
-const io = require( "socket.io" )();
+const io = require("socket.io")();
+const userModel = require('./routes/users')
 const socketapi = {
     io: io
 };
 
 // Add your socket.io logic here!
-io.on( "connection", function( socket ) {
-    console.log( "A user connected" );
+io.on("connection", function (socket) {
+    console.log("A user connected");
+
+    socket.on('join-server', async username => {
+
+        await userModel.findOneAndUpdate({
+            username
+        }, {
+            socketId: socket.id
+        })
+
+    })
+
+
+    socket.on('send-private-message', messageObject => {
+
+
+
+    })
+
 });
 // end of socket.io logic
 
